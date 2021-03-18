7f5b535c098346b9172fa26cf2b41e6389b62f62
Vandana Bhat <vbhat2@apple.com>
2021-03-17 22:10:06 -0700

    updating readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { DataGridComponent } from './data-grid/data-grid.component';
+import { DataCardComponent } from './data-card/data-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    DataGridComponent,
+    DataCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
